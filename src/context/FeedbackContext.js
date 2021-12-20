c41caddfc8b80f6a3ffd97809024e524cdf404c0
apichart vijittunukool <apichart@Apicharts-MacBook-Air.local>
2021-12-20 11:35:03 +0700

    3

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,7 +1,13 @@
-import { createContext, useState } from "react";
+import { createContext, useState,useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 const FeedbackContext = createContext();
 export const FeedbackProvider = ({ children }) => {
+  const [isLoading,setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([]);
+  const [feedbackEdit,setFeedbackEdit]= useState({
+    item:{},
+    edit:false
+  })
   const deleteFeedback = (id) => {
     if (window.confirm("Are you delete?")) {
       const newFeedback = feedback.filter((item) => item.id !== id);
@@ -17,15 +23,16 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback(feedback.map((val)=>(val.id===id?{...val,...newUpdate}:val)))
   }
 
-  const [feedback, setFeedback] = useState([
-    { id: 1, text: "feedback from context API state, id=1", rating: 6 },
-    { id: 2, text: "feedback from context API state, id=2", rating: 8 },
-    { id: 3, text: "feedback from context API state, id=3", rating: 10 },
-  ]);
-  const [feedbackEdit,setFeedbackEdit]= useState({
-    item:{},
-    edit:false
-  })
+  
+  useEffect(()=>{
+    fetchFeedback()
+  },[])
+  const fetchFeedback = async()=>{
+    const response = await fetch('http://localhost:4000/feedback?_sort=id&_order=desc')
+    const data = await response.json()
+    setFeedback(data)
+    setIsLoading(false)
+  }
   const editFeedback = (item)=>{
      setFeedbackEdit({
        item:item,
@@ -36,7 +43,10 @@ export const FeedbackProvider = ({ children }) => {
 
   // }
   return (
-    <FeedbackContext.Provider value={{ feedback, deleteFeedback ,addFeedback,editFeedback,feedbackEdit,updateFeedback,setFeedback}}>
+    <FeedbackContext.Provider value={
+      { feedback, deleteFeedback ,isLoading
+      ,addFeedback,editFeedback,feedbackEdit,
+      updateFeedback,setFeedback}}>
       {children}
     </FeedbackContext.Provider>
   );
